7e639cdadeeeb9239f52a56f6355b8139fce3a03
redlickigrzegorz <redlicki.grzegorz@gmail.com>
2017-02-10 11:42:15 +0100

    problem with permission to application for apache solved - part 2

--- support_center/wsgi.py
+++ support_center/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "support_center.settings")
 
 application = get_wsgi_application()
+cti = get_wsgi_application()
