bf874302dd3f70b6ca803721acb99505ecdb8bb0
redlickigrzegorz <redlicki.grzegorz@gmail.com>
2017-03-15 09:42:19 +0100

    new module for views with json returning

--- cti/urls.py
+++ cti/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from . import views
+from . import views, views_json
 
 
 app_name = 'cti'
@@ -15,15 +15,16 @@ urlpatterns = [
     url(r'^assign_to_me/(?P<fault_id>[0-9]+)/$', views.assign_to_me, name='assign_to_me'),
     url(r'^resolved_faults/$', views.resolved_faults, name='resolved_faults'),
     url(r'^change_password/$', views.change_password, name='change_password'),
-    url(r'^index_mobile/$', views.index_mobile, name='index_mobile'),
-    url(r'^login_mobile/$', views.login_mobile, name='login_mobile'),
-    url(r'^logout_mobile/$', views.logout_mobile, name='logout_mobile'),
-    url(r'^my_faults_mobile/$', views.my_faults_mobile, name='my_faults_mobile'),
-    url(r'^detail_mobile/(?P<fault_id>[0-9]+)/$', views.detail_mobile, name='detail_mobile'),
-    url(r'^add_fault_mobile/$', views.add_fault_mobile, name='add_fault_mobile'),
-    url(r'^edit_fault_mobile/(?P<fault_id>[0-9]+)/$', views.edit_fault_mobile, name='edit_fault_mobile'),
-    url(r'^delete_fault_mobile/(?P<fault_id>[0-9]+)/$', views.delete_fault_mobile, name='delete_fault_mobile'),
-    url(r'^assign_fault_mobile/(?P<fault_id>[0-9]+)/$', views.assign_to_me_mobile, name='assign_to_me_mobilei'),
-    url(r'^resolved_faults_mobile/$', views.resolved_faults_mobile, name='resolved_faults_mobile'),
-    url(r'^test/$', views.test, name='test'),
+    # mobile views
+    url(r'^index_mobile/$', views_json.index_mobile, name='index_mobile'),
+    url(r'^login_mobile/$', views_json.login_mobile, name='login_mobile'),
+    url(r'^logout_mobile/$', views_json.logout_mobile, name='logout_mobile'),
+    url(r'^my_faults_mobile/$', views_json.my_faults_mobile, name='my_faults_mobile'),
+    url(r'^detail_mobile/(?P<fault_id>[0-9]+)/$', views_json.detail_mobile, name='detail_mobile'),
+    url(r'^add_fault_mobile/$', views_json.add_fault_mobile, name='add_fault_mobile'),
+    url(r'^edit_fault_mobile/(?P<fault_id>[0-9]+)/$', views_json.edit_fault_mobile, name='edit_fault_mobile'),
+    url(r'^delete_fault_mobile/(?P<fault_id>[0-9]+)/$', views_json.delete_fault_mobile, name='delete_fault_mobile'),
+    url(r'^assign_fault_mobile/(?P<fault_id>[0-9]+)/$', views_json.assign_to_me_mobile, name='assign_to_me_mobilei'),
+    url(r'^resolved_faults_mobile/$', views_json.resolved_faults_mobile, name='resolved_faults_mobile'),
+    url(r'^test/$', views_json.test, name='test'),
 ]
