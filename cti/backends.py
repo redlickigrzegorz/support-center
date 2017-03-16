9c40064001538b7c4205e2a4b917d18a9b0a4124
redlickigrzegorz <redlicki.grzegorz@gmail.com>
2017-03-16 14:56:29 +0100

    better backend model for ldap authorization ready to use

--- cti/backends.py
+++ cti/backends.py
@@ -7,50 +7,65 @@ from support_center.settings import LDAP_AUTH_URL, LDAP_AUTH_SEARCH_BASE,\
 
 class LDAPBackend(object):
 
+    def __init__(self):
+        self.connection = None
+        self.user_data = ''
+
     def authenticate(self, username=None, password=None):
-        server = Server(LDAP_AUTH_URL)
+        pattern = r"^(\d+)@edu.p.lodz.pl$"
+        match = re.search(pattern, username)
 
-        c = Connection(server, user=LDAP_AUTH_CONNECTION_USERNAME, password=LDAP_AUTH_CONNECTION_PASSWORD)
-        c.open()
+        if match:
+            username = match.group(1)
 
-        if c.bind():
-            pattern = r"^(\d+)@edu.p.lodz.pl$"
-            match = re.search(pattern, username)
+        if self.make_connection():
+            self.get_user_data(username, password)
 
-            if match:
-                username = match.group(1)
-
-            user_search_filter = '(uid={})'.format(username)
-
-            if c.search(search_base=LDAP_AUTH_SEARCH_BASE,
-                        search_filter=user_search_filter,
-                        search_scope=SUBTREE):
-                common_name = c.response[0]['dn']
-
-                if c.rebind(user=common_name, password=password):
-                    try:
-                        user = User.objects.get(username=username)
-                    except User.DoesNotExist:
-                        user = User(username=username)
-
-                        pattern = r"cn=(\w+)\s+(\w+)\s+\d+"
-                        match = re.search(pattern, common_name)
-
-                        if match:
-                            user.first_name = match.group(1)
-                            user.last_name = match.group(2)
-                            user.email = "{}@edu.p.lodz.pl".format(username)
-                            user.is_superuser = False
-                            user.is_staff = False
-
-                        user.save()
-
-                    return user
+            return self.get_or_create_user(username)
 
         return None
 
-    def get_user(self, user_id):
+    def get_user(self, user):
         try:
-            return User.objects.get(pk=user_id)
+            return User.objects.get(pk=user)
         except User.DoesNotExist:
             return None
+
+    def make_connection(self):
+        server = Server(LDAP_AUTH_URL)
+
+        self.connection = Connection(server, user=LDAP_AUTH_CONNECTION_USERNAME, password=LDAP_AUTH_CONNECTION_PASSWORD)
+        self.connection.open()
+
+        return True if self.connection.bind() else False
+
+    def get_user_data(self, username, password):
+        user_search_filter = '(uid={})'.format(username)
+
+        if self.connection.search(search_base=LDAP_AUTH_SEARCH_BASE,
+                                  search_filter=user_search_filter,
+                                  search_scope=SUBTREE):
+            user_data = self.connection.response[0]['dn']
+
+            if self.connection.rebind(user=user_data, password=password):
+                self.user_data = user_data
+
+    def get_or_create_user(self, username):
+        try:
+            return User.objects.get(username=username)
+        except User.DoesNotExist:
+            user = User(username=username)
+
+            pattern = r"cn=(\w+)\s+(\w+)\s+(\d+)"
+            match = re.search(pattern, self.user_data)
+
+            if match:
+                user.first_name = match.group(1)
+                user.last_name = match.group(2)
+                user.email = "{}@edu.p.lodz.pl".format(match.group(3))
+                user.is_superuser = False
+                user.is_staff = False
+
+            user.save()
+
+            return user
