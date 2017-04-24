6dd20642e90e6d7515b766bd9323542f64c82aed
redlickigrzegorz <redlicki.grzegorz@gmail.com>
2017-04-24 12:32:08 +0200

    better option menu - more specific button in case of header

--- cti/static/cti/js/option_menu.js
+++ cti/static/cti/js/option_menu.js
@@ -2,30 +2,107 @@ $(document).ready(function() {
     $('.fault-tr').click(function(event) {
         $("#options-modal").width("0px");
         $("#options-modal").show();
-        var id = $(this).attr('id');
-        var content = $('<div class="options-menu">'+
-                            '<a href="/admin/fault_details/'+id+'" class="option-button details-button" title="details">'+
-                                '<div class="col-xs-3 col-sm-3 col-md-3 option">'+
-                                    '<span class="glyphicon glyphicon-file"></span>'+
-                                '</div>'+
-                            '</a>'+
-                            '<a href="/admin/edit_fault/'+id+'" class="option-button edit-button" title="edit">'+
-                                '<div class="col-xs-3 col-sm-3 col-md-3 option">'+
-                                    '<span class="glyphicon glyphicon-pencil"></span>'+
-                                '</div>'+
-                            '</a>'+
-                            '<a href="/admin/assign_to_me/'+id+'" class="option-button assign-button" title="assign to me">'+
-                                '<div class="col-xs-3 col-sm-3 col-md-3 option">'+
-                                    '<span class="glyphicon glyphicon-plus"></span>'+
-                                '</div>'+
-                            '</a>'+
-                            '<a href="/admin/delete_fault/'+id+'" class="option-button delete-button" title="delete">'+
-                                '<div class="col-xs-3 col-sm-3 col-md-3 option">'+
-                                    '<span class="glyphicon glyphicon-trash"></span>'+
-                                '</div>'+
-                            '</a>'+
-                        '</div>');
-        $("#options-modal").html(content).offset({ top: event.pageY, left: event.pageX}).animate({width: "200px"});
+        let header = $(".body-header span").text().trim();
+        let id = $(this).attr('id');
+
+        if (header === 'resolved faults') {
+            var content = $('<div class="options-menu">'+
+                                '<a class="option-button">'+
+                                    '<div class="col-xs-3 col-sm-3 col-md-3 option">'+
+                                        '<span class="glyphicon glyphicon-option-horizontal"></span>'+
+                                    '</div>'+
+                                '</a>'+
+                                '<a href="/admin/fault_details/'+id+'" class="option-button details-button" title="details">'+
+                                    '<div class="col-xs-3 col-sm-3 col-md-3 option">'+
+                                        '<span class="glyphicon glyphicon-file"></span>'+
+                                    '</div>'+
+                                '</a>'+
+                                '<a href="/admin/restore_fault/'+id+'" class="option-button restore-button" title="restore">'+
+                                    '<div class="col-xs-3 col-sm-3 col-md-3 option">'+
+                                        '<span class="glyphicon glyphicon-share"></span>'+
+                                    '</div>'+
+                                '</a>'+
+                                '<a class="option-button">'+
+                                    '<div class="col-xs-3 col-sm-3 col-md-3 option">'+
+                                        '<span class="glyphicon glyphicon-option-horizontal"></span>'+
+                                    '</div>'+
+                                '</a>'+
+                            '</div>');
+        }
+        else if (header === 'deleted faults') {
+            var content = $('<div class="options-menu">'+
+                                '<a class="option-button">'+
+                                    '<div class="col-xs-3 col-sm-3 col-md-3 option">'+
+                                        '<span class="glyphicon glyphicon-option-horizontal"></span>'+
+                                    '</div>'+
+                                '</a>'+
+                                '<a href="/admin/fault_details/'+id+'" class="option-button details-button" title="details">'+
+                                    '<div class="col-xs-3 col-sm-3 col-md-3 option">'+
+                                        '<span class="glyphicon glyphicon-file"></span>'+
+                                    '</div>'+
+                                '</a>'+
+                                '<a href="/admin/restore_fault/'+id+'" class="option-button restore-button" title="restore">'+
+                                    '<div class="col-xs-3 col-sm-3 col-md-3 option">'+
+                                        '<span class="glyphicon glyphicon-share"></span>'+
+                                    '</div>'+
+                                '</a>'+
+                                '<a class="option-button">'+
+                                    '<div class="col-xs-3 col-sm-3 col-md-3 option">'+
+                                        '<span class="glyphicon glyphicon-option-horizontal"></span>'+
+                                    '</div>'+
+                                '</a>'+
+                            '</div>');
+        }
+        else if (header === 'faults assigned to me') {
+            var content = $('<div class="options-menu">'+
+                                '<a href="/admin/fault_details/'+id+'" class="option-button details-button" title="details">'+
+                                    '<div class="col-xs-3 col-sm-3 col-md-3 option">'+
+                                        '<span class="glyphicon glyphicon-file"></span>'+
+                                    '</div>'+
+                                '</a>'+
+                                '<a href="/admin/edit_fault/'+id+'" class="option-button edit-button" title="edit">'+
+                                    '<div class="col-xs-3 col-sm-3 col-md-3 option">'+
+                                        '<span class="glyphicon glyphicon-pencil"></span>'+
+                                    '</div>'+
+                                '</a>'+
+                                '<a href="/admin/finish_fault/'+id+'" class="option-button finish-button" title="finish">'+
+                                    '<div class="col-xs-3 col-sm-3 col-md-3 option">'+
+                                        '<span class="glyphicon glyphicon-check"></span>'+
+                                    '</div>'+
+                                '</a>'+
+                                '<a href="/admin/delete_fault/'+id+'" class="option-button delete-button" title="delete">'+
+                                    '<div class="col-xs-3 col-sm-3 col-md-3 option">'+
+                                        '<span class="glyphicon glyphicon-trash"></span>'+
+                                    '</div>'+
+                                '</a>'+
+                            '</div>');
+        }
+        else {
+            var content = $('<div class="options-menu">'+
+                                '<a href="/admin/fault_details/'+id+'" class="option-button details-button" title="details">'+
+                                    '<div class="col-xs-3 col-sm-3 col-md-3 option">'+
+                                        '<span class="glyphicon glyphicon-file"></span>'+
+                                    '</div>'+
+                                '</a>'+
+                                '<a href="/admin/edit_fault/'+id+'" class="option-button edit-button" title="edit">'+
+                                    '<div class="col-xs-3 col-sm-3 col-md-3 option">'+
+                                        '<span class="glyphicon glyphicon-pencil"></span>'+
+                                    '</div>'+
+                                '</a>'+
+                                '<a href="/admin/assign_to_me/'+id+'" class="option-button assign-button" title="assign to me">'+
+                                    '<div class="col-xs-3 col-sm-3 col-md-3 option">'+
+                                        '<span class="glyphicon glyphicon-plus"></span>'+
+                                    '</div>'+
+                                '</a>'+
+                                '<a href="/admin/delete_fault/'+id+'" class="option-button delete-button" title="delete">'+
+                                    '<div class="col-xs-3 col-sm-3 col-md-3 option">'+
+                                        '<span class="glyphicon glyphicon-trash"></span>'+
+                                    '</div>'+
+                                '</a>'+
+                            '</div>');
+        }
+
+        $("#options-modal").html(content).offset({ top: event.pageY, left: event.pageX}).animate({width: "160px"});
     });
 
     $(document).click(function(event) {
