a59b443583f4c5304420692566f3d5e9aca2ac89
redlickigrzegorz <redlicki.grzegorz@gmail.com>
2017-05-15 11:48:00 +0200

    more tests for admin view

--- cti/tests.py
+++ cti/tests.py
@@ -181,6 +181,7 @@ class ViewTest(TestCase):
 class JsonViewTest(TestCase):
 
     def setUp(self):
+
         self.user = User.objects.create_user(username='000000', password='password')
 
         self.fault = Fault(issuer='000000',
@@ -275,3 +276,174 @@ class JsonViewTest(TestCase):
         response = self.client.get(reverse('cti:user_details_json', args=(self.user.id,)))
 
         self.assertEqual(response.status_code, 200)
+
+
+class AdminViewTest(TestCase):
+
+    def setUp(self):
+
+        self.superuser = User.objects.create_superuser(username='000000', email='000000@gmail.com', password='password')
+        self.user = User.objects.create_user(username='111111', password='password')
+
+        self.fault = Fault(issuer='111111',
+                           handler='000000',
+                           object_number='0000000000',
+                           topic='topic',
+                           description='description',
+                           phone_number='000000000',
+                           status=0,
+                           priority=1,
+                           is_visible=True,
+                           watchers='[]')
+        self.fault.save()
+
+        self.fault_object = Object(object_number='0000000000',
+                                   object_name='object name',
+                                   date=datetime.date.today(),
+                                   room='room',
+                                   status=1,
+                                   price=2.00,
+                                   comments='comments')
+        self.fault_object.save()
+
+        self.client.login(username='000000', password='password')
+
+    def test_call_view_for_index_admin(self):
+        response = self.client.get(reverse('cti:index_admin'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'cti/admin/index.html')
+
+    def test_call_view_for_my_faults_admin(self):
+        response = self.client.get(reverse('cti:my_faults_admin'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'cti/admin/index.html')
+
+    def test_call_view_for_watched_faults_admin(self):
+        response = self.client.get(reverse('cti:watched_faults_admin'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'cti/admin/index.html')
+
+    def test_call_view_for_resolved_faults_admin(self):
+        response = self.client.get(reverse('cti:resolved_faults_admin'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'cti/admin/index.html')
+
+    def test_call_view_for_searched_faults_admin(self):
+        response = self.client.get(reverse('cti:searched_faults_admin'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'cti/admin/index.html')
+
+    def test_call_view_for_deleted_faults_admin(self):
+        response = self.client.get(reverse('cti:deleted_faults_admin'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'cti/admin/index.html')
+
+    def test_call_view_for_all_users_admin(self):
+        response = self.client.get(reverse('cti:all_users_admin'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'cti/admin/users.html')
+
+    def test_call_view_for_all_history_admin(self):
+        response = self.client.get(reverse('cti:all_history_admin'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'cti/admin/history.html')
+
+    def test_call_view_for_edit_fault_admin(self):
+        response = self.client.get(reverse('cti:edit_fault_admin', args=(self.fault.id,)))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'cti/admin/fault_form.html')
+
+    def test_call_view_for_watch_unwatch_fault_admin(self):
+        response = self.client.get(reverse('cti:watch_fault_admin', args=(self.fault.id,)))
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_call_view_for_finish_fault_admin(self):
+        response = self.client.get(reverse('cti:finish_fault_admin', args=(self.fault.id,)))
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_call_view_for_delete_fault_admin(self):
+        response = self.client.get(reverse('cti:delete_fault_admin', args=(self.fault.id,)))
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_call_view_for_assign_to_me_admin(self):
+        response = self.client.get(reverse('cti:assign_to_me_admin', args=(self.fault.id,)))
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_call_view_for_reassign_fault_admin(self):
+        response = self.client.get(reverse('cti:reassign_fault_admin', args=(self.fault.id, self.superuser.username,)))
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_call_view_for_restore_fault_admin(self):
+        response = self.client.get(reverse('cti:restore_fault_admin', args=(self.fault.id,)))
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_call_view_for_edit_user_admin(self):
+        response = self.client.get(reverse('cti:edit_user_admin', args=(self.superuser.id,)))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'cti/admin/user_form.html')
+
+    def test_call_view_for_block_user_admin(self):
+        response = self.client.get(reverse('cti:block_user_admin', args=(self.superuser.id,)))
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_call_view_for_restore_user_admin(self):
+        response = self.client.get(reverse('cti:restore_user_admin', args=(self.superuser.id,)))
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_call_view_for_change_password_admin(self):
+        response = self.client.get(reverse('cti:change_password_admin'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'cti/admin/change_password.html')
+
+    def test_call_view_for_fault_details_admin(self):
+        response = self.client.get(reverse('cti:fault_details_admin', args=(self.fault.id,)))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'cti/admin/fault_details.html')
+
+    def test_call_view_for_object_details_admin(self):
+        response = self.client.get(reverse('cti:object_details_admin', args=(self.fault_object.object_number,)))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'cti/admin/object_details.html')
+
+    def test_call_view_for_user_details_admin(self):
+        response = self.client.get(reverse('cti:user_details_admin', args=(self.superuser.id,)))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'cti/admin/user_details.html')
+
+    def test_call_view_for_ask_for_reassign_admin(self):
+        response = self.client.get(reverse('cti:ask_for_reassign_admin', args=(self.fault.id, self.superuser.username,)))
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_call_view_for_report_phone_number_admin(self):
+        response = self.client.get(reverse('cti:report_phone_number_admin', args=(self.fault.id,)))
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_call_view_for_statistics_admin(self):
+        response = self.client.get(reverse('cti:statistics_admin'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'cti/admin/statistics.html')
